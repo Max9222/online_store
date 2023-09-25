0610fc7ed284654d7adde7b5abf286a38fe45a58
Max9222 <maxoog@bk.ru>
2023-09-25 22:15:27 +0300

    This is all

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     phone = request.POST.get('phone')
+    #     message = request.POST.get('message')
+    #     print(f'{name} ({phone}): {message}')
 
     return render(request, 'catalog/index.html')
 
 
+def index_1(request):
+
+    return render(request, 'catalog/index_1.html')
