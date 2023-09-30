778c8351d9f2846a05ab7a93b740db5b22685daf
Max9222 <maxoog@bk.ru>
2023-09-30 20:12:42 +0300

    add

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=250, verbose_name='категория')
+    purchase_price = models.IntegerField(verbose_name='цена за покупку')
+    date_of_creation = models.DateTimeField()
+    last_modified_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'наименование'
+        verbose_name_plural = 'наименования'
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.CharField(max_length=100, verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'наименование'
+        verbose_name_plural = 'наименования'
+        ordering = ('name',)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from django.utils.translation.trans_real import catalog
 
@@ -6,4 +8,4 @@ from catalog.views import index, contact
 urlpatterns = [
     path('', index),
     path('contacts/', contact)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
