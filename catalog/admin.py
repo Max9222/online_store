bb95d436bf085193245f677bb329442f3e1cfe14
Max9222 <maxoog@bk.ru>
2023-09-30 21:11:53 +0300

    add

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,9 +5,14 @@ from catalog.models import Product, Category
 #admin.site.register(Product)
 #admin.site.register(Category)
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'purchase_price', 'category',)  #
+    list_display = ('id', 'name', 'purchase_price', 'category',)  #
     list_filter = ('category',) #
     search_fields = ('name', 'description',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
 
