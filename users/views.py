75942e3c4ce065831d31466fb8416031655436c4
Max9222 <maxoog@bk.ru>
2023-10-24 23:57:35 +0300

    Домашка 22.2 Задание 2 (Не все)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
-# Create your views here.
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+
+    def form_valid(self, form):
+        new_user = form.save
+        send_mail(
+            subject='fghfgh',
+            message='fghfg',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email],
+            #auth_password=[new_user.password],
+        )
+        return super().form_valid(form)
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
