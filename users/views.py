28516010c89436b9dfe1030dd9ee0c96b3fc238a
Max9222 <maxoog@bk.ru>
2023-10-25 23:47:29 +0300

    Домашка 22.2 Задание 2 - готово

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
+import random
+from random import randint
 from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserRegisterForm, UserProfileForm
@@ -15,15 +18,13 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('users:login')
     template_name = 'users/register.html'
 
-
     def form_valid(self, form):
-        new_user = form.save
+        self.object = form.save()
         send_mail(
-            subject='fghfgh',
-            message='fghfg',
+            subject='Mass',
+            message='New',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[new_user.email],
-            #auth_password=[new_user.password],
+            recipient_list=[self.object.email]
         )
         return super().form_valid(form)
 
@@ -35,3 +36,16 @@ class ProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Смена пароля',
+        message=f'Новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('users:profile'))
